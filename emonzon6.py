61a2040ed1d7e06134c79872cc596f3c8ab8ad20
Edvin23 <150404642+Edvin23@users.noreply.github.com>
2024-05-27 19:13:21 -0700

    Add files via upload

---
+++ emonzon6.py
@@ -0,0 +1,28 @@
+#Edvin Monzon
+#CSC 321
+#Lab 6
+
+x = 5.5
+
+if x>5.2:
+    print ("x is greater than 5.2")
+else:
+    print("x is less than 5.2")
+
+
+y = 7
+
+if y == 8:
+    print("Y is 8")
+    print("This is the second statemnt for the selection")
+elif y > 8:
+    print("y is greater than 8")
+else:
+    print("y is less than 8")
+
+z = "StarWars"
+
+if z == "StarWars":
+    print("Variable z is StarWars")
+else:
+    print("Variable z is not StarWars")
